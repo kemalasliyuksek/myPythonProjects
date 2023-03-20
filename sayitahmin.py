1640a82dbfc30b6f7e62fcb51c99f61f64c2164b
Kemal Aslıyüksek <kemalasliyuksek@icloud.com>
2023-03-20 13:13:22 +0300

    Update sayitahmin.py

--- sayitahmin.py
+++ sayitahmin.py
@@ -2,19 +2,17 @@ import random as r
 rastgelesayi = r.randint(1,100)
 print("-- SAYI TAHMİN OYUNU --")
 print("\n1 ile 100 arasında bir sayı belirlendi.")
+islem = 0
 while True:
     tahmin = int(input("\nSayıyı Tahmin Edin: "))
     if tahmin == rastgelesayi:
-        print("\n******************")
-        print("* Doğru Bildiniz *")
-        print("******************")
-        print("Doğru Sayı : ", rastgelesayi)
-        print("******************")
-        print("""Görüş ve Öneriler için :
-Instagram : @kemalasliyuksek
-Twitter : @kemalasliyuksek
-Email : kemalaslyksk53@gmail.com""")
+        islem = islem + 1
+        islemstr = str(islem)
+        print("\nTebrikler. ", islemstr + ". tahminde doğru sayıya (", rastgelesayi, ") ulaştınız.")
+        print("\nGörüş ve Öneriler için : \nInstagram : @kemalasliyuksek \nTwitter : @kemalasliyuksek \nEmail : kemalaslyksk53@gmail.com")
+
     elif tahmin < rastgelesayi:
+        islem = islem + 1
         print("\n---------------------------")
         print("-- Daha Büyük Sayı Girin --")
         print("---------------------------")
@@ -22,6 +20,7 @@ Email : kemalaslyksk53@gmail.com""")
         print("\n---------------------------")
         print("-- Daha Küçük Sayı Girin --")
         print("---------------------------")
+        islem = islem + 1
     elif tahmin > 100:
         print("\nGirdiğiniz sayı 100'den küçük olmalıdır!")
     elif tahmin < 0:
